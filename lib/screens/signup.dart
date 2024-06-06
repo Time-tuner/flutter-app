ac4bc1df0a0de063414c1b7ea2a5e71cebf564c1
Rei-Suzuki1729 <eva2021end@gmail.com>
2024-06-06 14:58:28 +0900

    6/6 SignUp
    Co-authored-by: Alice8261120 <Alice8261120@users.noreply.github.com>
    Co-authored-by: sirakawagou <sirakawagou@users.noreply.github.com>

---
+++ lib/screens/signup.dart
@@ -0,0 +1,24 @@
+//新規登録画面を作る
+import 'package:flutter/material.dart';
+
+
+
+
+
+
+//メアドとパスワード入力画面
+
+//例えばの話
+// class SignUp extends StatelessWidget {
+//   const SignUp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const Scaffold(
+//       body: SignUpForm(),
+//     );
+//   }
+// }
+
+
+
