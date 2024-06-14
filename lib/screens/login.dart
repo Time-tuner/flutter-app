e313afdcbc5b3809ff9e05883d6ef1631e719174
vzkun <vzkun115@gmail.com>
2024-06-14 10:32:09 +0900

    Co-authored-by: haruto0213 <haruto0213@users.noreply.github.com> Co-authored-by: sirakawagou <sirakawagou@users.noreply.github.com> Co-authored-by: Akane_Watanabe <akane02320@users.noreply.github.com> Co-authored-by: Alice8261120 <Alice8261120@users.noreply.github.com>

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:sign_in_button/sign_in_button.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
+
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -19,51 +22,196 @@ class _LoginPageState extends State<LoginPage> {
     ));
   }
 
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff0092ac),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(30.0),
+      body: Container(
+        child: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Divider(),
-              Center(
-                child: SignInButtonBuilder(
-                  text: 'メールアドレスでログイン',
-                  textColor: Color(0xff555555),
-                  icon: Icons.email,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Stack(
+                children: [
+                  Positioned(
+                    left: -50,
+                    top: -40,
+                    child: Image.asset(
+                      'images/cloud1.png',
+                      width: 200,
+                      height: 200,
+                    ),
+                  ),
+                  Positioned(
+                    left: 300,
+                    top: 0,
+                    child: Image.asset(
+                      'images/cloud1.png',
+                      width: 300,
+                      height:200,
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                      ),
+                      SizedBox(height: 100),
+                      Text(
+                        'Timetuner',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 50),
+              SizedBox(
+                width: 250,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    foregroundColor: Colors.white, backgroundColor: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset("images/google_logo.png"),
+                      ),
+                      SizedBox(width: 20),
+                      Text(
+                        'Googleでログイン',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 250,
+                height: 50,
+                child: ElevatedButton(
                   onPressed: () {
-                    _showButtonPressDialog(context, 'Email');
+                    //ボタンが押されたときの処理
                   },
-                  backgroundColor: Color(0xfffafafa),
-                  width: 220.0,
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                child: TextFormField(
-                  obscureText: _isObscure,
-                  decoration: InputDecoration(
-                      labelText: 'Password',
-                      suffixIcon: IconButton(
-                          icon: Icon(_isObscure
-                              ? Icons.visibility_off
-                              : Icons.visibility),
-                          onPressed: () {
-                            setState(() {
-                              _isObscure = !_isObscure;
-                            });
-                          })
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    foregroundColor: Colors.white, backgroundColor: Color(0xFF4cc764),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      Positioned.fill(
+                        child: Material(
+                          color: Colors.transparent,
+                          child: InkWell(
+                            onTap: (){
+                              //ボタンが押されたときの処理
+                            },
+                          child: Image.asset(
+                        'images/line_logo.png', // Lineのロゴ画像をassetsフォルダに追加してください
+                        fit: BoxFit.contain,
+                      ),
                           ),
+                        ),
+                      ),
+                     
+                      SizedBox(width: 30),
+                      Text(
+                        'Lineでログイン',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              Center(
-                child: ElevatedButton(onPressed: () {}, child: Text('ログイン')),
+              SizedBox(height: 20),
+              SizedBox(
+                width: 250,
+                height: 50,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.zero,
+                    foregroundColor: Colors.black, backgroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      SizedBox(width: 5),
+                      ClipRRect(
+                        child: Image.asset(
+                          "images/mail_logo.png",
+                          width:40,
+                          height:30,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'メールアドレスでログイン',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ],
+                  ),
+                ),
               ),
+              SizedBox(height: 30),
+              Stack(
+                alignment: Alignment.topCenter,
+                children: [
+                  Positioned(
+                    child: Image.asset(
+                          "images/ground2.png",
+                          width: 600,
+                    )
+                  ),
+                  Column(
+                    children: [
+                      SizedBox(height: 50),
+                      Center(
+                        child:Text(
+                          'パスワードを忘れた場合はこちら',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      Center(
+                        child:Text(
+                          '新規登録の方はこちら',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(height: 50), // 下部のスペース
             ],
           ),
         ),
